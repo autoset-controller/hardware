c101e7434a7cb941497729656caa4649df086558
Matt Hollenbeck <mdhollen@asu.edu>
2017-10-06 19:38:31 -0700

    Added COM Package Building
    Updated J5-SerialInterface.js so that it can interpret Serial input from
    multiple radios at the same time, and convert the inputs into
    pseudo-packets. This file does not yet send a UDP packet to an IP
    address.

--- 2. Hardware Interface/Sample Code/J5-SerialInterface.js
+++ 2. Hardware Interface/Sample Code/J5-SerialInterface.js
@@ -1,7 +1,63 @@
-var serialport = require('serialport');
-var port = new serialport('COM9');
+let serialport = require('serialport');
+let port = new serialport('COM9');
+
+let store = false;
+let label = ''; //tracks label of input radio
+let breaker = ''; //tracks proper formatting
+let value = ''; //tracks value of input
+let inputCount = 0;
 
 // Switches the port into "flowing mode"
 port.on('data', function (data) {
-    console.log('Data:', data);
+    let buffSize = data.byteLength;
+    let i;
+    for (i = 0; i < buffSize; i++) {
+        let c = String.fromCharCode(data[i]);
+        if(c == '\n') {
+            //if input is valid and value is an int
+            if(store == true && !isNaN(parseInt(value))) {
+                //output new package
+                let packageString = 'New Package: \nFrom Radio ' + label + '\n';
+                packageString += 'Value = ' + value;
+                console.log(packageString);
+            }
+            else {
+                console.log('invalid serial input: ' + label + breaker + value);
+            }
+            //reset values
+            label = '';
+            breaker = '';
+            value = '';
+            store = false;
+        }
+        if(label == '') {
+            if(c == 'A' || c == 'B') {
+                label = c;
+                //console.log('Label: ' + label);
+            }
+            else {
+
+            }
+        }
+        else if(breaker == '' || breaker == ':') {
+            breaker += c;
+            //console.log('Breaker: ' + breaker);
+        }
+        else {
+            //if c is not a space, carriage return, or newline
+            if(c != '\n' && data[i] != 10 && data[i] != 13) {
+                //if value contains 3 or fewer chars and c is a digit
+                if(value.length < 4 && !isNaN(parseInt(c))) {
+                    value += c;
+                    store = true;
+                    //console.log('Value: ' + value);
+                }
+                else {
+                    value += c;
+                    store = false;
+                    //console.log('Invalid Value: ' + value);
+                }
+            }
+        }
+    }
 });
