690ce96a879c4f3d9a16cf95f030c86c09788bbe
Matt Hollenbeck <mdhollen@asu.edu>
2018-03-12 22:10:48 -0700

    Updated Main Programs
    motorMain.c now processes generic packets and E_STOP packets. Additions:
    accel_time, motor updates, finalized interface for motor controllers
    using UART serial.
    
    encoderMain.c now has more efficient packet processing, using dynamic
    update sizing to prevent issues with interrupt overloads when updates
    need to be send to the central control.

--- 2. Hardware Interface/Mains/encoderMain.c
+++ 2. Hardware Interface/Mains/encoderMain.c
@@ -15,9 +15,22 @@
 #define phaseBPinL 3
 
 //system info
+#define XBEE_BAUD 57600
+#define UPDATE_SIZE_MAX = 128;
+#define UPDATE_SIZE_MIN = 16;
+#define MATCH_LIMIT = 16;
 double TICKS_PER_CYCLE = 4096;
+
+int update_size = 64 //minimum # updates to trigger a packet send
+int update_match = 0;
 double radius = .25 * .3048; //radius of encoder wheels (m)
 double base = 1 * .3048; //distance from encoder wheels to center (m)
+
+//forward decl
+void phaseA_R();
+void phaseB_R();
+void phaseA_L();
+void phaseB_L();
 
 //interrupt info
 double dR;
@@ -39,6 +52,119 @@ char checksum_str[128];
 int serialPort;
 int update;
 
+//Test program that send the position of a platform driving in circles.
+int main(int argc, char* argv)  {
+
+	DEBUG("Encoder Test:\n");
+
+	wiringPiSetup();
+	serialPort = serialOpen("/dev/ttyUSB0", XBEE_BAUD);
+
+	//Initialize encoder pins
+	pinMode(phaseAPinL, INPUT);
+	pinMode(phaseBPinL, INPUT);
+	pinMode(phaseAPinR, INPUT);
+	pinMode(phaseBPinR, INPUT);
+
+	//Initialize function for encoder interrupts
+	int phaseAFuncR = wiringPiISR(phaseAPinR, INT_EDGE_BOTH, phaseA_R);
+	int phaseBFuncR = wiringPiISR(phaseBPinR, INT_EDGE_BOTH, phaseB_R);
+	int phaseAFuncL = wiringPiISR(phaseAPinL, INT_EDGE_BOTH, phaseA_L);
+	int phaseBFuncL = wiringPiISR(phaseBPinL, INT_EDGE_BOTH, phaseB_L);
+
+	x = 0; y = 0; theta = 0;
+	dx = 0; dy = 0; dtheta = 0;
+
+	double dR_in, dL_in;
+	int updates_in;
+
+	int wait = 0;
+
+	while(1) {
+		//Check for serial data
+		if(serialDataAvail(serialPort) > 0) {
+			while(serialDataAvail(serialPort) > 0) {
+				//TODO: Process serial input
+				//SET_POSITION: Set x, y, and theta to incoming position data
+				//MODIFY_ATTRIBUTE: Set key, platform, radius, or base to incoming data
+				DEBUG("%c", (char)serialGetchar(serialPort));
+			}
+		}
+
+		//calculate change in x,y,theta
+		if(update >= update_size) {
+			//DEBUG("updates: %d\n", update);
+			//Process update and clear transfer values
+			dR_in = dR;
+			dL_in = dL;
+			updates_in = update;
+
+			dR = 0;
+			dL = 0;
+			update = 0;
+
+			//interrupts can now safely modify these values
+			DEBUG("%d: %f, %f\n", updates_in, dR_in, dL_in);
+
+			//update update size to process updates efficiently
+			if(update <= update_size) { // if updates were processed without delay
+				update_match++;
+				//if updates are consistently processed without delay, attempt to lower the update size
+				if(update_match >= match_limit) {
+					update_match = 0;
+					if(update_size >= UPDATE_SIZE_MIN * 2) {
+						update_size /= 2;
+					}
+					else if(update_size >= UPDATE_SIZE_MIN) {
+						update_size -= 1;
+					}
+				}
+			}
+			else { //if updates were processed with some delay
+				if(update_size <= UPDATE_SIZE_MAX / 2) {
+					update_size *= 2;
+				}
+				else if(update_size <= UPDATE_SIZE_MAX) {
+					update_size += 1;
+				}
+			}
+
+			dR_in = dR_in * 2 * M_PI / TICKS_PER_CYCLE;
+			dL_in = dL_in * 2 * M_PI / TICKS_PER_CYCLE;
+
+			dtheta = (radius / (2 * base)) * (dR_in - dL_in);
+			theta = theta + dtheta;
+			theta = fmod(theta, 2 * M_PI);
+
+			// |x| = |x0| + |cos(theta) cos(theta)| * |dR| = |x0| + r/2 * |cos(theta)*dR + cos(theta) * dL|
+			// |y|   |y0|   |sin(theta) sin(theta)|   |dL|   |y0| +       |sin(theta)*dR + sin(theta) * dL|
+
+			// x = x0 + r/2 * cos(theta) * (dR + dL)
+			// y = y0 + r/2 * sin(theta) * (dR + dL)
+
+			x = x + radius / 2 * cos(theta) * (dR_in + dL_in);
+			y = y + radius / 2 * sin(theta) * (dR_in + dL_in);
+
+			checksum = theta + x;
+			checksum = checksum + y;
+
+			//format position data for serial output
+			sprintf(x_str, "%f", x);
+			sprintf(y_str, "%f", y);
+			sprintf(theta_str, "%f", theta);
+			sprintf(checksum_str, "%f", checksum);
+
+			char packet[128];
+			sprintf(packet, "1234|0|%s|%s|%s|%s\n", x_str, y_str, theta_str, checksum_str);
+
+			//DEBUG("sending packet: %s", packet);
+			serialPuts(serialPort, packet);
+		}
+	}
+
+	return 0;
+}
+
 //INTERRUPT FUNCTIONS
 void phaseA_R() {
 	A_R = digitalRead(phaseAPinR);
@@ -55,11 +181,10 @@ void phaseA_R() {
 		dR += 1; //Move position clockwise
 	}
 	else {
-		dR += TICKS_PER_CYCLE - 1; //Move position counterclockwise
+		dR -= 1; //Move position counterclockwise
 	}
-
-	dR = fmod(dR, TICKS_PER_CYCLE);
 	update++;
+	//DEBUG("dR = %f\n", dR);
 	//printf("pos = %d\n", pos);
 }
 
@@ -78,11 +203,10 @@ void phaseB_R() {
 		dR += 1; //Move position clockwise
 	}
 	else {
-		dR += TICKS_PER_CYCLE - 1; //Move position counterclockwise
+		dR -= 1; //Move position counterclockwise
 	}
-
-	dR = fmod(dR, TICKS_PER_CYCLE);
 	update++;
+	//DEBUG("dR = %f\n", dR);
 	//printf("pos = %d\n", pos);
 }
 
@@ -101,11 +225,10 @@ void phaseA_L() {
 		dL += 1; //Move position clockwise
 	}
 	else {
-		dL += TICKS_PER_CYCLE - 1; //Move position counterclockwise
+		dL -= 1; //Move position counterclockwise
 	}
-
-	dL = fmod(dL, TICKS_PER_CYCLE);
 	update++;
+	//DEBUG("dL = %f\n", dL);
 	//printf("pos = %d\n", pos);
 }
 
@@ -124,83 +247,9 @@ void phaseB_L() {
 		dL += 1; //Move position clockwise
 	}
 	else {
-		dL += TICKS_PER_CYCLE - 1; //Move position counterclockwise
+		dL -= 1; //Move position counterclockwise
 	}
-
-	dL = fmod(dL, TICKS_PER_CYCLE);
 	update++;
+	//DEBUG("dL = %f\n", dL);
 	//printf("pos = %d\n", pos);
-}
-
-//Test program that send the position of a platform driving in circles.
-int main(int argc, char* argv)  {
-
-	DEBUG("Encoder Test:\n");
-
-	wiringPiSetup();
-	serialPort = serialOpen("/dev/ttyUSB0", 38400);
-
-	//Initialize encoder pins
-	pinMode(phaseAPinL, INPUT);
-	pinMode(phaseBPinL, INPUT);
-	pinMode(phaseAPinR, INPUT);
-	pinMode(phaseBPinR, INPUT);
-
-	//Initialize function for encoder interrupts
-	int phaseAFuncR = wiringPiISR(phaseAPinR, INT_EDGE_BOTH, phaseA_R);
-	int phaseBFuncR = wiringPiISR(phaseBPinR, INT_EDGE_BOTH, phaseB_R);
-	int phaseAFuncL = wiringPiISR(phaseAPinL, INT_EDGE_BOTH, phaseA_L);
-	int phaseBFuncL = wiringPiISR(phaseBPinL, INT_EDGE_BOTH, phaseB_L);
-
-	x = 0; y = 0; theta = 0;
-	dx = 0; dy = 0; dtheta = 0;
-
-	while(1) {
-		//Check for serial data
-		if(serialDataAvail(serialPort) > 0) {
-			while(serialDataAvail(serialPort) > 0) {
-				DEBUG("%c", (char)serialGetchar(serialPort));
-			}
-		}
-		//DEBUG("%f, %f\n", dR, dL);
-
-		//calculate change in x,y,theta
-		if(update != 0) {
-			DEBUG("updates: %d\n", update);
-			dtheta = (radius / (2 * base)) * (dR - dL);
-			theta = theta + dtheta;
-			theta = fmod(theta, 2 * M_PI);
-
-			// |x| = |x0| + |cos(theta) cos(theta)| * |dR| = |x0| + r/2 * |cos(theta)*dR + cos(theta) * dL|
-			// |y|   |y0|   |sin(theta) sin(theta)|   |dL|   |y0| +       |sin(theta)*dR + sin(theta) * dL|
-
-			// x = x0 + r/2 * cos(theta) * (dR + dL)
-			// y = y0 + r/2 * sin(theta) * (dR + dL)
-
-			x = x + radius / 2 * cos(theta) * (dR + dL);
-			y = y + radius / 2 * sin(theta) * (dR + dL);
-
-			checksum = theta + x;
-			checksum = checksum + y;
-
-			update = 0;
-			dR = 0;
-			dL = 0;
-
-			//format position data for serial output
-			sprintf(x_str, "%f", x);
-			sprintf(y_str, "%f", y);
-			sprintf(theta_str, "%f", theta);
-			sprintf(checksum_str, "%f", checksum);
-
-			char packet[128];
-			sprintf(packet, "1234|0|%s|%s|%s|%s\n", x_str, y_str, theta_str, checksum_str);
-
-			DEBUG("sending packet: %s", packet);
-			serialPuts(serialPort, packet);
-			//delay(500);
-		}
-	}
-
-	return 0;
 }
--- 2. Hardware Interface/Mains/motorMain.c
+++ 2. Hardware Interface/Mains/motorMain.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,23 +8,33 @@
 #define debug 1
 #define DEBUG(args ...) if(debug) { printf(args); }
 
-#define pwmPinL 1
-#define pwmPinR 26
-#define dirPinL 0
-#define dirPinR 2
+#define addr_l 128
+#define addr_r 135
+
+#define active_l 0
+#define active_r 1
+
+#define MOTOR_MIN 0
+#define MOTOR_MAX 127
+
+#define PACKET_ARGS 8
 
 //Forward Decl
+void getSerialData();
 void updateSpeeds();
 void clearLine(int);
 void clearPacket();
 void writeMotors();
+void sendMotorPacket(int,int,int,int);
 
 //Serial Port Values
-int serialPort;
+int serialXBee;
+int serialMotor;
 
 //Motor Active Values
 int targetL, targetR;
 int speedL, speedR;
+int accelDelayL, accelDelayR;
 
 //Motor Limits
 int minL, minR;
@@ -42,6 +53,7 @@ char target_in_str[128] = "\0";
 char id_in_str[128] = "\0";
 char target_L_in_str[128] = "\0";
 char target_R_in_str[128] = "\0";
+char accel_time_in_str[128] = "\0";
 char checkSum_in_str[128] = "\0";
 
 int key_in; //value should be positive int matching key
@@ -50,6 +62,7 @@ int target_in; //value should be positive int matching type
 int id_in;
 int target_L_in; //value should be int in range of -1024 to 1024
 int target_R_in; //value should be int in range of -1024 to 1024
+int accel_time_in; //value should be int greater than 0
 int checkSum_in; //value should match sum of target_L_in and target_R_in
 
 int valuesLoaded = 0;
@@ -58,18 +71,15 @@ int update = 0;
 
 int main(int argc, char* argv)  {
 
+	DEBUG("Booting Motor Controllers...\n");
+	delay(3000);
+	DEBUG(" Done\n");
+
 	DEBUG("Starting Motor Brain...\n");
 
 	wiringPiSetup();
-	serialPort = serialOpen("/dev/ttyUSB0", 38400);
-
-	//Initialize pins
-	pinMode(pwmPinL, PWM_OUTPUT);
-	pinMode(pwmPinR, PWM_OUTPUT);
-	pinMode(dirPinL, OUTPUT);
-	pinMode(dirPinR, OUTPUT);
-
-	//Initialize serial strings
+	serialXBee = serialOpen("/dev/ttyUSB0", 57600);
+	serialMotor = serialOpen("/dev/ttyS0", 9600);
 
 	//Initialize values
 	targetL = 0;
@@ -78,190 +88,40 @@ int main(int argc, char* argv)  {
 	speedL = 0;
 	speedR = 0;
 
-	minL = 80;
-	minR = 80;
+	accelDelayL = 0;
+	accelDelayR = 0;
 
-	maxL = 1024;
-	maxR = 1024;
+	minL = 0;
+	minR = 0;
+
+	maxL = MOTOR_MAX;
+	maxR = MOTOR_MAX;
+
+	DEBUG("Activating Motors...\n");
+	sendMotorPacket(addr_l, 0, 0, 0);
+	sendMotorPacket(addr_r, 0, 0, 0);
+
+	digitalWrite(active_l, HIGH);
+	digitalWrite(active_r, HIGH);
+
 
 	//Initialize serial
 
-	if(serialPort != -1) {
+	if(serialXBee != -1 && serialMotor != -1) {
 		DEBUG("serial init successful.\n");
 	}
 	else {
 		DEBUG("serial init failed.\n");
 	}
-	serialFlush(serialPort);
+	serialFlush(serialXBee);
 
+	DEBUG("Motor Brain Started.\n");
+
+	//MAIN LOOP
 	while(1) {
 		//Process all data in buffer
-		if(serialDataAvail(serialPort) > 0) {
-			while(serialDataAvail(serialPort) > 0) {
-				char c = (char)serialGetchar(serialPort);
-				DEBUG("%c", c);
-				if(c == '|') {
-					valuesLoaded += 1;
-					//preemptively ignore packets
-					switch(valuesLoaded) {
-						case 1:
-							key_in = atoi(key_in_str);
-							if(key_in != key) {
-								DEBUG("ignoring packet: key mismatch\n");
-								clearLine(serialPort);
-								clearPacket();
-							}
-							break;
-						case 2:
-							plat_in = atoi(plat_in_str);
-							if(plat_in != platform) {
-								DEBUG("ignoring packet: platform mismatch\n");
-								clearLine(serialPort);
-								clearPacket();
-							}
-							break;
-						case 3:
-							target_in = atoi(target_in_str);
-							if(target_in != type) {
-								DEBUG("ignoring packet: type mismatch\n");
-								clearLine(serialPort);
-								clearPacket();
-							}
-							break;
-					}
-				}
-				else if(c == '\n') {
-					//Display packet info
-					DEBUG("key_in: %s\n", key_in_str);
-					DEBUG("plat_in: %s\n", plat_in_str);
-					DEBUG("target_in: %s\n", target_in_str);
-					DEBUG("id_in: %s\n", id_in_str);
-					DEBUG("target_L_in: %s\n", target_L_in_str);
-					DEBUG("target_R_in: %s\n", target_R_in_str);
-					DEBUG("checkSum_in: %s\n", checkSum_in_str);
-
-					//process all inputs
-					int ignore = 0;
-					int isBad = 0;
-
-					//If fewer than 4 values have been loaded, we cannot tell if this packet was sent from the controller.
-					if(valuesLoaded < 4) {
-						//ignore packet
-						DEBUG("too few values loaded, ignoring: %d\n", valuesLoaded);
-						ignore = 1;
-					}
-					//Otherwise, we need to check and see if the values are valid.
-					else {
-						//Commented tests have already been checked
-						/*
-						//key should be equal to key stored in system. Otherwise, ignore packet.
-						key_in = atoi(key_in_str);
-						if(key_in != key) {
-							ignore = 1;
-							DEBUG("key does not match: %d != %d\n", key_in, key);
-						}
-						//platform should be equal to platform id stored in system. Otherwise, ignore packet.
-						plat_in = atoi(plat_in_str);
-						if(plat_in != platform) {
-							ignore = 1;
-							DEBUG("platform does not match: %d != %d\n", plat_in, platform);
-						}
-						//target should be equal to type stored in system. Otherwise, ignore packet.
-						target_in = atoi(target_in_str);
-						if(target_in != type) {
-							ignore = 1;
-							DEBUG("type does not match: %d != %d\n", target_in, type);
-						}*/
-						//target_L should be a number between -1024 and 1024. Otherwise, packet is bad.
-						target_L_in = atoi(target_L_in_str);
-						if(target_L_in < -1024 || target_L_in > 1024) {
-							isBad = 1;
-							DEBUG("target_L out of bounds: %d\n", target_L_in);
-						}
-						//target_R should be a number between -1024 and 1024. Otherwise, packet is bad.
-						target_R_in = atoi(target_R_in_str);
-						if(target_R_in < -1024 || target_R_in > 1024) {
-							isBad = 1;
-							DEBUG("target_R out of bounds: %d\n", target_R_in);
-						}
-						//checkSum should be equal to the value of target_L_in + target_R_in. Otherwise, packet is bad.
-						checkSum_in = atoi(checkSum_in_str);
-						if(checkSum_in != target_L_in + target_R_in) {
-							isBad = 1;
-							DEBUG("checkSum does not match: %d != %d + %d\n", checkSum_in, target_L_in, target_R_in);
-						}
-					}
-					if(!ignore) {
-						char badString[8] = "bad:";
-						char goodString[8] = "good:";
-						char* pString;
-						if(isBad) {
-							strcat(badString, id_in_str);
-							pString = badString;
-						}
-						else {
-							strcat(goodString, id_in_str);
-							pString = goodString;
-							//set motor values
-							targetL = target_L_in;
-							targetR = target_R_in;
-							update = 1;
-						}
-						//acknowledge packet
-						DEBUG("sending packet %s...\n", pString);
-						serialPuts(serialPort, pString);
-					}
-					else {
-						DEBUG("ignoring packet\n");
-					}
-					clearPacket();
-				}
-				else {
-					char tmp[2] = "\0\0";
-					tmp[0] = c;
-
-					switch(valuesLoaded) {
-						//process key entry
-						case 0:
-							strcat(key_in_str, tmp); //add next char to key
-							break;
-
-						//process platform entry
-						case 1:
-							strcat(plat_in_str, tmp); //add next char to platform
-							break;
-
-						//process target entry
-						case 2:
-							strcat(target_in_str, tmp); //add next char to target
-							break;
-
-						case 3:
-							strcat(id_in_str, tmp); //add next char to id
-							break;
-
-						//process target_L entry
-						case 4:
-							strcat(target_L_in_str, tmp); //add next char to target_L
-							break;
-
-						//process target_R entry
-						case 5:
-							strcat(target_R_in_str, tmp); //add next char to target_R
-							break;
-
-						//process checkSum entry
-						case 6:
-							strcat(checkSum_in_str, tmp); //add next char to checksum
-							break;
-
-						//all values processed
-						default:
-
-							break;
-					}
-				}
-			}
+		if(serialDataAvail(serialXBee) > 0) {
+			getSerialData();
 		}
 		//Set speeds closer to target speeds
 		if(update > 0) {
@@ -269,8 +129,202 @@ int main(int argc, char* argv)  {
 			writeMotors();
 		}
 	}
-
 	return 0;
+}
+
+//PROCESS SERIAL INPUT:
+//Processes any waiting serial input and loads it into a packet
+//Standard packet: sets target speeds to target_R and target_L, sets corresponding accelDelay.
+//E_STOP: sets target speeds, speeds, and accelDelay to 0, writes values to motors.
+//TODO: ATTRIBUTES_MODIFY: modify key or platform according to inputs
+void getSerialData() {
+	while(serialDataAvail(serialXBee) > 0) {
+		char c = (char)serialGetchar(serialXBee);
+		DEBUG("%c", c);
+		if(c == '|') {
+			valuesLoaded += 1;
+			//preemptively ignore packets
+			switch(valuesLoaded) {
+				case 1:
+					key_in = atoi(key_in_str);
+					if(key_in != key) {
+						DEBUG("ignoring packet: key mismatch\n");
+						clearLine(serialXBee);
+						clearPacket();
+					}
+					break;
+				case 2:
+					plat_in = atoi(plat_in_str);
+					if(plat_in != platform) {
+						if(strcmp(target_in, "E_STOP")) {
+							//Set all values to stop values
+							target_L = 0;
+							target_R = 0;
+							speedL = 0;
+							speedR = 0;
+							accelDelayL = 0;
+							accelDelayR = 0;
+							writeMotors();
+							DEBUG("emergency stop processed\n");
+						}
+						else {
+							DEBUG("ignoring packet: platform mismatch\n");
+						}
+						clearLine(serialXBee);
+						clearPacket();
+					}
+					break;
+				case 3:
+					target_in = atoi(target_in_str);
+					if(target_in != type) {
+						DEBUG("ignoring packet: type mismatch\n");
+						clearLine(serialXBee);
+						clearPacket();
+					}
+					break;
+			}
+		}
+		//PROCESS COMPLETE PACKET:
+		//Packet structure:
+		//key|platform|target|id|target_L|target_R|accel_time|checkSum
+		//key: key lock for establishing valid packet sending
+		//platform: id of target platform: must match current platform id
+		//target: target brain: 0 = motor brain, 1 = encoder brain
+		//id: id of packet - used for handshake
+		//target_L, target_R: target speeds for each motor
+		//accel_time: time (in seconds) to accelerate to target speeds
+		//checkSum: validation value == target_L + target_R + accel_time
+		else if(c == '\n') {
+			//Display packet info
+			DEBUG("key_in: %s\n", key_in_str);
+			DEBUG("plat_in: %s\n", plat_in_str);
+			DEBUG("target_in: %s\n", target_in_str);
+			DEBUG("id_in: %s\n", id_in_str);
+			DEBUG("target_L_in: %s\n", target_L_in_str);
+			DEBUG("target_R_in: %s\n", target_R_in_str);
+			DEBUG("accel_time_in: %s\n", accel_time_in_str);
+			DEBUG("checkSum_in: %s\n", checkSum_in_str);
+
+			//process all inputs
+			int ignore = 0;
+			int isBad = 0;
+
+			//If fewer than 4 values have been loaded, we cannot tell if this packet was sent from the controller.
+			if(valuesLoaded < 4) {
+				//ignore packet
+				DEBUG("too few values loaded, ignoring: %d\n", valuesLoaded);
+				ignore = 1;
+			}
+			//Otherwise, we need to check and see if the values are valid.
+			else {
+				//target_L should be a number smaller than the MOTOR_MAX. Otherwise, packet is bad.
+				target_L_in = atoi(target_L_in_str);
+				if(target_L_in < -MOTOR_MAX || target_L_in > MOTOR_MAX) {
+					isBad = 1;
+					DEBUG("target_L out of bounds: %d\n", target_L_in);
+				}
+				//target_R should be a number smaller than the MOTOR_MAX. Otherwise, packet is bad.
+				target_R_in = atoi(target_R_in_str);
+				if(MOTOR_MAX < -1024 || target_R_in > MOTOR_MAX) {
+					isBad = 1;
+					DEBUG("target_R out of bounds: %d\n", target_R_in);
+				}
+				accel_time_in = atoi(accel_time_in_str);
+				if(accel_time_in < 0) {
+					isBad = 1;
+					DEBUG("accel_time out of bounds: %d\n", accel_time_in);
+				}
+				//checkSum should be equal to the value of target_L_in + target_R_in. Otherwise, packet is bad.
+				checkSum_in = atoi(checkSum_in_str);
+				if(checkSum_in != target_L_in + target_R_in + accel_time_in) {
+					isBad = 1;
+					DEBUG("checkSum does not match: %d != %d + %d + %d\n", checkSum_in, target_L_in, target_R_in, accel_time_in);
+				}
+			}
+			if(!ignore) {
+				char badString[8] = "bad:";
+				char goodString[8] = "good:";
+				char* pString;
+				if(isBad) {
+					strcat(badString, id_in_str);
+					pString = badString;
+				}
+				else {
+					strcat(goodString, id_in_str);
+					pString = goodString;
+					//set motor values
+					targetL = target_L_in;
+					targetR = target_R_in;
+					float diffR, diffL;
+					diffL = abs(speedL - targetL);
+					diffR = abs(speedR - targetR);
+					float accelDelay = (float)accel_time_in * 1000.0 * 2 / (diffL + diffR);
+					printf("delay: %f\n", accelDelay);
+					accelDelayL = (float)accelDelay * diffL / (diffL + diffR);
+					accelDelayR = (float)accelDelay * diffR / (diffL + diffR);
+					printf("left delay: %d, right delay %d\n", accelDelayL, accelDelayR);
+					update = 1;
+				}
+				//acknowledge packet
+				DEBUG("sending packet %s...\n", pString);
+				serialPuts(serialXBee, pString);
+			}
+			else {
+				DEBUG("ignoring packet\n");
+			}
+			clearPacket();
+		}
+		else {
+			char tmp[2] = "\0\0";
+			tmp[0] = c;
+
+			switch(valuesLoaded) {
+				//process key entry
+				case 0:
+					strcat(key_in_str, tmp); //add next char to key
+					break;
+
+				//process platform entry
+				case 1:
+					strcat(plat_in_str, tmp); //add next char to platform
+					break;
+
+				//process target entry
+				case 2:
+					strcat(target_in_str, tmp); //add next char to target
+					break;
+
+				case 3:
+					strcat(id_in_str, tmp); //add next char to id
+					break;
+
+				//process target_L entry
+				case 4:
+					strcat(target_L_in_str, tmp); //add next char to target_L
+					break;
+
+				//process target_R entry
+				case 5:
+					strcat(target_R_in_str, tmp); //add next char to target_R
+					break;
+
+				//process target_R entry
+				case 6:
+					strcat(accel_time_in_str, tmp); //add next char to target_R
+					break;
+
+				//process checkSum entry
+				case 7:
+					strcat(checkSum_in_str, tmp); //add next char to checksum
+					break;
+
+				//all values processed
+				default:
+
+					break;
+			}
+		}
+	}
 }
 
 ///PIN FUNCTIONS:
@@ -293,28 +347,44 @@ void updateSpeeds() {
 		speedR -= 1;
 		update += 1;
 	}
-	DEBUG("targets: %d | %d\n", targetL, targetR);
+	//DEBUG("targets: %d | %d\n", targetL, targetR);
 }
 
 //Sets motor speeds to current speed values
 void writeMotors() {
 	if(speedL > 0) {
-		pwmWrite(pwmPinL, speedL);
-		digitalWrite(dirPinL, 0);
+		sendMotorPacket(addr_l, 0, speedL, accelDelayL);
+		//pwmWrite(pwmPinL, speedL);
+		//digitalWrite(dirPinL, 0);
 	}
 	else {
-		pwmWrite(pwmPinL, -speedL);
-		digitalWrite(dirPinL, 1);
+		sendMotorPacket(addr_l, 1, -speedL, accelDelayL);
+		//pwmWrite(pwmPinL, -speedL);
+		//digitalWrite(dirPinL, 1);
 	}
 	if(speedR > 0) {
-		pwmWrite(pwmPinR, speedR);
-		digitalWrite(dirPinR, 0);
+		sendMotorPacket(addr_r, 0, speedR, accelDelayR);
+		//pwmWrite(pwmPinR, speedR);
+		//digitalWrite(dirPinR, 0);
 	}
 	else {
-		pwmWrite(pwmPinR, -speedR);
-		digitalWrite(dirPinR, 1);
+		sendMotorPacket(addr_r, 1, -speedR, accelDelayR);
+		//pwmWrite(pwmPinR, -speedR);
+		//digitalWrite(dirPinR, 1);
 	}
 	DEBUG("setting speeds: %d | %d\n", speedL, speedR);
+}
+
+void sendMotorPacket(int address, int command, int speed, int accelDelay) {
+	serialPutchar(serialMotor, (char)address);
+	serialPutchar(serialMotor, (char)command);
+	serialPutchar(serialMotor, (char)speed);
+	int valid = (address + command + speed) & 0b0111111;
+	serialPutchar(serialMotor, (char)valid);
+	DEBUG("sending comm %d at speed %d to %d\n", command, speed, address);
+	if(accelDelay > 0) {
+		delay(accelDelay);
+	}
 }
 
 ///PACKET FUNCTIONS:
@@ -334,6 +404,7 @@ void clearPacket() {
 	id_in_str[0] = '\0';
 	target_L_in_str[0] = '\0';
 	target_R_in_str[0] = '\0';
+	accel_time_in_str[0] = '\0';
 	checkSum_in_str[0] = '\0';
 
 	valuesLoaded = 0;
